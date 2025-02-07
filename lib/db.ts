0ba965a20047e8556e924196a82459bb80f49d66
mylena-dlc <mylena.delacote@laposte.net>
2025-02-07 21:41:20 +0100

    mise en ligne

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,11 @@
-// Importing PrismaClient from the @prisma/client package
 import { PrismaClient } from "@prisma/client";
- 
-// Declaring a global variable prisma with type PrismaClient or undefined
-declare global {
-    var prisma: PrismaClient | undefined;
-}
- 
-// Exporting db which either uses the global prisma instance or creates a new PrismaClient instance
-export const db = globalThis.prisma || new PrismaClient();
- 
-// If not in production environment, assigning the db instance to the global prisma variable
+
+// Vérifier si Prisma existe déjà dans globalThis
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+// Utiliser l'instance existante ou en créer une nouvelle
+export const db = globalForPrisma.prisma ?? new PrismaClient();
+
 if (process.env.NODE_ENV !== "production") {
-    // Use `let` here instead of `var` to declare a global variable
-    globalThis.prisma = db;
+  globalForPrisma.prisma = db;
 }
